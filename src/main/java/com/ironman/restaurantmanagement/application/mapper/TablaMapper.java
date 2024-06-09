1c0f360fd267e851c4baecfb76f331619ac01377
iisaiaso <isaiasibarraonofre@gmail.com>
2024-06-08 22:10:31 -0500

    add mapper

--- src/main/java/com/ironman/restaurantmanagement/application/mapper/TablaMapper.java
+++ src/main/java/com/ironman/restaurantmanagement/application/mapper/TablaMapper.java
@@ -1,4 +1,21 @@
 package com.ironman.restaurantmanagement.application.mapper;
 
+import com.ironman.restaurantmanagement.application.dto.tabla.TablaBodyDto;
+import com.ironman.restaurantmanagement.application.dto.tabla.TablaDto;
+import com.ironman.restaurantmanagement.application.dto.tabla.TablaSaveDto;
+import com.ironman.restaurantmanagement.application.dto.tabla.TablaSmallDto;
+import com.ironman.restaurantmanagement.presistence.entity.Tabla;
+import org.mapstruct.MappingTarget;
+
 public interface TablaMapper {
+
+    TablaDto   toDto(Tabla tabla);
+
+    TablaSmallDto toSmallDto(Tabla tabla);
+
+    TablaSaveDto toSaveDto(Tabla tabla);
+
+    Tabla entity(TablaBodyDto tablaBodyDto);
+
+    Tabla updateEntity(@MappingTarget Tabla tabla, TablaBodyDto tablaBodyDto);
 }
