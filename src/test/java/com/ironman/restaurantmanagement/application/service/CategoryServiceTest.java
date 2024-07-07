388644ed51119fae53b128a0b386225e54ec7955
AbelPalominoRojas <abel.palominorojas@gmail.com>
2024-07-06 23:25:10 -0500

    test (categories): add notFound test

--- src/test/java/com/ironman/restaurantmanagement/application/service/CategoryServiceTest.java
+++ src/test/java/com/ironman/restaurantmanagement/application/service/CategoryServiceTest.java
@@ -23,9 +23,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
@@ -192,6 +190,20 @@ class CategoryServiceTest {
         assertNotNull(categorySaved);
         assertEquals(category.getId(), categorySaved.getId());
         assertEquals(State.DISABLED.getValue(), categorySaved.getState().getValue());
+    }
+
+    @Test
+    void findByIdNotFound() {
+        // Given
+        Long categoryId = 100L;
+        String expectedMessage = "Category not found with id: " + categoryId;
+
+        // When
+        Exception exception = assertThrows(DataNotFoundException.class, () -> categoryService.findById(categoryId));
+        String actualMessage = exception.getMessage();
+
+        // Then
+        assertEquals(expectedMessage, actualMessage);
     }
 
 }
