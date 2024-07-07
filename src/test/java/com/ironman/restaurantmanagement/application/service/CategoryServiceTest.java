aab69a5be552ebc5ff75f65b527b8f1e40f82260
AbelPalominoRojas <abel.palominorojas@gmail.com>
2024-07-06 22:35:25 -0500

    test (categories): use Spy, InjectMocks and reflect

--- src/test/java/com/ironman/restaurantmanagement/application/service/CategoryServiceTest.java
+++ src/test/java/com/ironman/restaurantmanagement/application/service/CategoryServiceTest.java
@@ -2,24 +2,31 @@ package com.ironman.restaurantmanagement.application.service;
 
 import com.ironman.restaurantmanagement.application.dto.category.CategoryDto;
 import com.ironman.restaurantmanagement.application.dto.category.CategorySmallDto;
-import com.ironman.restaurantmanagement.application.mapper.CategoryMapper;
+import com.ironman.restaurantmanagement.application.mapper.CategoryMapperImpl;
 import com.ironman.restaurantmanagement.application.service.impl.CategoryServiceImpl;
 import com.ironman.restaurantmanagement.presistence.entity.Category;
 import com.ironman.restaurantmanagement.presistence.repository.CategoryRepository;
 import com.ironman.restaurantmanagement.shared.exception.DataNotFoundException;
 import com.ironman.restaurantmanagement.shared.state.enums.State;
+import com.ironman.restaurantmanagement.shared.state.mapper.StateMapperImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,18 +34,20 @@ class CategoryServiceTest {
     @Mock
     private CategoryRepository categoryRepository;
 
-    @Mock
-    private CategoryMapper categoryMapper;
+    @Spy
+    private StateMapperImpl stateMapper;
 
-    private CategoryService categoryService;
+    @Spy
+    private CategoryMapperImpl categoryMapper;
+
+    @InjectMocks
+    private CategoryServiceImpl categoryService;
 
     Long id;
     Category category;
 
     @BeforeEach
     void setUp() {
-        categoryService = new CategoryServiceImpl(categoryRepository, categoryMapper);
-
         id = 1L;
         category = Category.builder()
                 .id(id)
@@ -50,7 +59,15 @@ class CategoryServiceTest {
                 .updatedAt(LocalDateTime.now())
                 .build();
 
-
+        List<Field> categoryMapperFields = List.of(categoryMapper.getClass().getDeclaredFields());
+        categoryMapperFields
+                .stream()
+                .filter(field -> "stateMapper".equals(field.getName()))
+                .findFirst()
+                .ifPresent(field ->{
+                    ReflectionUtils.makeAccessible(field);
+                    ReflectionUtils.setField(field, categoryMapper, stateMapper);
+                });
 
     }
 
@@ -62,14 +79,6 @@ class CategoryServiceTest {
 
         when(categoryRepository.findAll())
                 .thenReturn(List.of(category));
-
-        CategorySmallDto categorySmallDto = CategorySmallDto.builder()
-                .id(id)
-                .name(category.getName())
-                .build();
-
-        when(categoryMapper.toSmallDto(category))
-                .thenReturn(categorySmallDto);
 
         // When
 
@@ -85,30 +94,19 @@ class CategoryServiceTest {
     @Test
     void findById() throws DataNotFoundException {
         // Given
-        when(categoryRepository.findById(id))
+        when(categoryRepository.findById(anyLong()))
                 .thenReturn(Optional.of(category));
-
-        CategoryDto categoryDto = CategoryDto.builder()
-                .id(id)
-                .name(category.getName())
-                .description(category.getDescription())
-                .urlKey(category.getUrlKey())
-                .state(State.ENABLED)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        when(categoryMapper.toDto(category))
-                .thenReturn(categoryDto);
 
         // When
         CategoryDto categoryFound = categoryService.findById(id);
 
         // Then
         assertNotNull(categoryFound);
-        assertEquals(1L, categoryFound.getId());
+        assertEquals(category.getId(), categoryFound.getId());
         assertEquals(category.getName(), categoryFound.getName());
-
+        assertEquals(category.getDescription(), categoryFound.getDescription());
+        assertEquals(category.getUrlKey(), categoryFound.getUrlKey());
+        assertEquals(category.getState(), categoryFound.getState().getValue());
     }
 
 }
